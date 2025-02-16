365aedd8135ff9eddc2aced29b12752b83b13376
CodeshShubh <codeshshubh@gmail.com>
2025-02-16 06:45:25 +0530

    lecture6

---
+++ 3_how_node_works/index.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+
+
+// if we have blocking oprations
+console.log(1)
+console.log(2)
+console.log(3)
+
+const result = fs.readFileSync('text.txt', 'utf-8')
+console.log('blocking output :',result)
+
+console.log(4)
+console.log(5)
+console.log(6)
+console.log(7)
+
+
+
+
+
+// if we have non  blocking oprations
+console.log(1)
+console.log(2)
+console.log(3)
+
+fs.readFile('text.txt', 'utf-8',(err, result)=>{
+    if(err){
+        console.log(err)
+    }else{
+        console.log( 'nonblcking output :',result)
+    }
+})
+
+
+console.log(4)
+console.log(5)
+console.log(6)
+console.log(7)
