cbc0b3ba39b428cb0769779958944a84d8d08c8d
CodeshShubh <codeshshubh@gmail.com>
2025-04-01 16:00:15 +0530

    readline input output

---
+++ 2.1_readline/fs_readline.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const readline = require('readline');
+
+
+const rl = readline.createInterface({input:process.stdin , output:process.stdout});
+
+
+rl.question("Enter your text to save in a file", (input)=>{
+    fs.writeFile('userFile.txt', input , (err)=>{
+       if(err){
+          console.log('error While creating a file', err)
+       }
+       console.log("file is created with your given input")
+
+       fs.readFile('userFile.txt', 'utf-8' , (err,data)=>{
+            if(err) console.log('error while reading file' ,err)
+               console.log("UserFile content" , data)
+
+            rl.question("Do you want to delete File?(yes/no)",(input)=>{
+                 if(input.toLowerCase()==="yes"){
+                   fs.unlink('userFile.txt',(err)=>{
+                      if(err) console.log('error while deleting file:' , err)
+                         console.log('file Deleted Succesfully')
+                        rl.close()
+                   });
+                 }else{
+                   console.log("file Kept!");
+                   rl.close()
+                 }
+            })
+       })
+    })
+
+})
